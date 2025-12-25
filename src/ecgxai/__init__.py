33516d51e6f35aa41410ff21fc1401030d93c352
Naresh <rajnareshwar@gmail.com>
2025-12-25 21:11:18 +0000

    Change imports to relative in __init__.py

--- src/ecgxai/__init__.py
+++ src/ecgxai/__init__.py
@@ -1,9 +1,9 @@
-from config import MAXLEN
-from preprocessing import preprocess_for_model, load_mat_TF
-from ecg_lime import run_lime_for_one_class_from_sel
-from payload import payload_from_lime_row
-from plot import plot_from_payload
-from explainer import run_explain_for_one_class, run_pipeline_for_classes
-from segments import *
-from eval import evaluate_explanation, REGISTRY
-from utils import save_run, load_run
+from .config import MAXLEN
+from .preprocessing import preprocess_for_model, load_mat_TF
+from .ecg_lime import run_lime_for_one_class_from_sel
+from .payload import payload_from_lime_row
+from .plot import plot_from_payload
+from .explainer import run_explain_for_one_class, run_pipeline_for_classes
+from .segments import *  # noqa: F401,F403
+from .eval import evaluate_explanation, REGISTRY
+from .utils import save_run, load_run
